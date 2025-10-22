b8e9e3df6d840182f02c173c6841f9f1bec710b3
8-bit-confusion <lilywasko@gmail.com>
2025-10-22 16:27:51 -0400

    refactored to use controller class

---
+++ AnagramsGameController.php
@@ -0,0 +1,106 @@
+<?php
+
+class AnagramsGameController {
+    private $db;
+    private $context;
+    
+    private $shuffledString;
+
+    public function __construct() {
+        session_start();
+
+        // get input from appropriate request context
+        $this->context = match($_SERVER['REQUEST_METHOD']) {
+            'GET' => $_GET,
+            'POST' => $_POST,
+        };
+    }
+
+    public function run() {
+        $command = $context['command'];
+        
+        match ($command) {
+            'welcome', default => $this->welcome();
+            'login' => $this->login();
+            'start-game' => $this->startGame();
+            'process-guess' => $this->proccessGuess();
+            'game-over' => $this->gameover();
+        }
+    }
+
+    // COMMAND FUNCTIONS ###########################################################################################
+
+    public function welcome($passwordIncorrect = false) {
+        include "./welcome.html";
+        if ($passwordIncorrect) {
+            echo '<script>document.getElementById("password-incorrect").style.display = "block";</script>';
+        }
+    }
+
+    public function login() {
+        $username = $context['username'];
+        $email = $context['email'];
+        $password = $context['password'];
+        
+        // handle login
+
+        // if (password is wrong)
+        $this->welcome(true);
+    }
+
+    public function startGame() {
+        include "./game.html";
+    }
+
+    public function processGuess() {
+        $guess = $context["guess"];
+        if (strlen($guess) != 7) {
+            // Change echos later
+            echo "Guess is invalid";
+        }
+        if (!$this->validLetters($guess, $shuffledString))
+            echo "Guess is invalid";
+        else {
+            include "./game-over.html";
+        }
+    }
+
+    public function gameover() {
+        include "./game-over.html";
+    }
+
+    // PRIVATE FUNCTIONS ###########################################################################################
+
+    private function validLetters($guess, $word) {
+        $guess_chars = str_split($guess);
+        $word_chars = str_split($word);
+
+        sort($guess_chars);
+        sort( $word_chars );
+
+        return ($guess_chars == $word_chars);
+    }
+
+    private function validWord($guess) {
+        $dictFile = "./words7.txt";
+        $dict = file_get_contents($dictFile);
+        
+        $words = preg_split("/\R", $dict);
+        
+    }
+
+    // note from lily to lilli---removed return
+    // value here since refactoring means we can just
+    // set a class variable
+    private function chooseShuffledString() {
+        // Replace with "/var/www/html/homework/words7.txt"
+        // when moving pages to server
+        $dictFile = "./words7.txt";
+        $dict = file_get_contents($dictFile);
+        
+        $words = preg_split("/\R", $dict);
+        $this->shuffledString = str_shuffle($words[array_rand($words)]);
+    }
+}
+
+?>
--- index.php
+++ index.php
@@ -1,85 +1,21 @@
 <?php
 
-$context = NULL;
-switch ($_SERVER['REQUEST_METHOD']) {
-    case 'POST':
-        $context = $_POST;
-        break;
-    
-    case 'GET':
-        $context = $_GET;
-        break;
-}
+// DEBUG: print errors
+// (from trivia example)
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 
-$shuffledString = NULL;
+// autoload classes from src/
+// OUR AnagramsGameController.php FILE WILL BE DEPLOYED IN
+// OPT/SRC, NOT IN A PUBLIC FOLDER
+// (from trivia example)
+spl_autoload_register(function ($classname) {
+    include "$classname.php";
+});
 
-$command = $context['command'];
-switch ($command) {
-    case 'welcome':
-    default:
-        include "./welcome.html";
-        break;
+// instantiate and run controller
+// (from trivia example)
+$controller = new AnagramsGameController($_GET);
+$controller->run();
 
-    case 'login':
-        $username = $context['username'];
-        $email = $context['email'];
-        $password = $context['password'];
-        
-        // handle login
-        // if (password is wrong)
-        include "./welcome.html";
-        echo '<script>document.getElementById("password-incorrect").style.display = "block";</script>';
-        break;
-    
-    case 'game':
-        include "./game.html";
-        break;
-
-    case 'guess':
-        $guess = $context["guess"];
-        if (strlen($guess) != 7) {
-            // Change echos later
-            echo "Guess is invalid";
-        }
-        if (!validLetters($guess, $shuffledString))
-            echo "Guess is invalid";
-        else {
-            include "./game-over.html";
-        }
-        break;
-
-    case 'game-over':
-        include "./game-over.html";
-        break;
-}
-
-function validLetters($guess, $word){
-    $guess_chars = str_split($guess);
-    $word_chars = str_split($word);
-
-    sort($guess_chars);
-    sort( $word_chars );
-
-    return ($guess_chars == $word_chars);
-}
-
-function validWord($guess) {
-    $dictFile = "./words7.txt";
-    $dict = file_get_contents($dictFile);
-    
-    $words = preg_split("/\R", $dict);
-    
-}
-
-function chooseShuffledString(): String{
-    // Replace with "/var/www/html/homework/words7.txt"
-    // when moving pages to server
-    $dictFile = "./words7.txt";
-    $dict = file_get_contents($dictFile);
-    
-    $words = preg_split("/\R", $dict);
-    global $shuffledString;
-    $shuffledString = str_shuffle($words[array_rand($words)]);
-    return $shuffledString;
-}
 ?>
